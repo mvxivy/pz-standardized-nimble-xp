1c4f9038a49724a83efa6e125e9d721f4135b060
Maksim Ivanov <vonavimax@gmail.com>
2025-01-23 02:02:31 +0700

    fix: default option for delay

--- 42/media/lua/client/modOptions.lua
+++ 42/media/lua/client/modOptions.lua
@@ -3,8 +3,6 @@ local config = StandardizedNimbleXP_config
 local function InitConfig() 
 	local options = PZAPI.ModOptions:create(config.ModId, config.ModName)
 
-	options:addDescription(getText("UI_options_STNIMBLE_B42_multiplier_description"))
-
 	config.ComboBoxMultiplier = options:addComboBox(
 		"combobox1",
 		getText("UI_options_STNIMBLE_B42_multiplier_label")
@@ -17,7 +15,8 @@ local function InitConfig()
 	config.ComboBoxMultiplier:addItem("100x", false)
 	config.ComboBoxMultiplier:addItem("1000x", false)
 
-	options:addDescription(getText("UI_options_STNIMBLE_B42_delay_description"))
+	options:addDescription(getText("UI_options_STNIMBLE_B42_multiplier_description"))
+	
 
 	config.ComboBoxDelay = options:addComboBox(
 		"combobox2",
@@ -25,10 +24,12 @@ local function InitConfig()
 	)
 
 	config.ComboBoxDelay:addItem("250", false)
-	config.ComboBoxDelay:addItem("500", true)
-	config.ComboBoxDelay:addItem("1000", false)
+	config.ComboBoxDelay:addItem("500", false)
+	config.ComboBoxDelay:addItem("1000", true)
 	config.ComboBoxDelay:addItem("2500", false)
 	config.ComboBoxDelay:addItem("5000", false)
+
+	options:addDescription(getText("UI_options_STNIMBLE_B42_delay_description"))
 end
 
 InitConfig()
